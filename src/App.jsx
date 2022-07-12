58163de2a0e0b1e577c511fb9826157b8a27d343
muratagm <108925501+muratagm@users.noreply.github.com>
2022-07-12 17:11:23 +0900

    propsの分割代入 //分割代入することで、いちいちpropsと書かなくても良くなる

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,7 @@ import ColorfulMessage from "./components/ColorfulMessage"; //拡張子はしょ
 const App = () => {
   const onClickButton = () => alert();
 
-  //オブジェクトの変数を定義
-  const contentLedyStyle = {
-    //※1 これはjavascriptのブジェクト
-    color: "blue",
-    fontSize: "18px",
-    fontWeight: "bold", //ここはjavascriptのオブジェクト野中なので、cssのハイフンはキャメルケースで確必要がある
-    backgroundColor: "pink"
-  };
   return (
-    //コンポーネント化したやつをインポートしてきて、出力するときはタグの中に凝っンポーねんと名を確<colorfulMessage />タグは閉じないとエラーになる
-    //コンポーネントにてきとうな名前をつけて例：color="blue" message="おげんきですか" (color、messageがpropsになる)
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue" fontSize="24px" backgroundColor="skyblue">
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  //propsは引数、名前は何でもいいがpropsとしたほうがわかりやすい
-  console.log(props);
-  //コンソールで確認すると、App.jsxでかいたpropsが渡ってきている ({color: "blue", message: "おげんきですか"})
-  //コンポーネントのなかでそれを使うように書いていく {color: "blue", message: "おげんきですか"}
+  //分割代入することで、いちいちpropsと書かなくても良くなる
+  const { color, fontSize, backgroundColor, children } = props;
   const contentStyle = {
-    //※1 これはjavascriptのブジェクト
-    color: props.color,
-    fontSize: props.fontSize,
-    fontWeight: "bold", //ここはjavascriptのオブジェクト野中なので、cssのハイフンはキャメルケースで確必要がある
-    backgroundColor: props.backgroundColor
+    color: color,
+    fontSize: fontSize,
+    fontWeight: "bold",
+    backgroundColor: backgroundColor
   };
 
-  return <p style={contentStyle}>{props.children}</p>; //props コンポーネントに特定のめいしょうをつけて渡すこともできるし、タグで囲って(コンポーネントタグでくくると)childrenという変数で渡すこともできる
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;

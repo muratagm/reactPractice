62e68252575a43bf6151876fe33e9c7fdd81f493
muratagm <108925501+muratagm@users.noreply.github.com>
2022-07-12 17:24:00 +0900

    props 分割代入 分割代入することで、いちいちpropsと書かなくても良くなる

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  console.log(props);
   //分割代入することで、いちいちpropsと書かなくても良くなる
   const { color, fontSize, backgroundColor, children } = props;
   const contentStyle = {
-    color: color,
+    color, //color: color, javascriptはプロパティ名と当てはめるやつ(2行上でpropsから分割代入したやつ)が同じ名称だと省略して表記することができる
     fontSize: fontSize,
     fontWeight: "bold",
     backgroundColor: backgroundColor

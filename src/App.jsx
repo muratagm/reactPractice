d22e12e56b151458e851e3d9f919191ddb46eedb
muratagm <108925501+muratagm@users.noreply.github.com>
2022-07-12 16:30:41 +0900

    style Reactでのスタイルの扱い

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 const App = () => {
   const onClickButton = () => alert();
+
+  //オブジェクトの変数を定義
+  const contentStyle = {
+    //※1 これはjavascriptのブジェクト
+    color: "blue",
+    fontSize: "18px",
+    fontWeight: "bold", //ここはjavascriptのオブジェクト野中なので、cssのハイフンはキャメルケースで確必要がある
+    backgroundColor: "skyblue"
+  };
   return (
     <>
       <h1
@@ -8,12 +17,13 @@ const App = () => {
           //波括弧 javascript その中で更に波括弧 「javascriptのオブジェクト」になる
           //cssのプロパティをかくにはストリング文字列として扱う必要がある
           //文字列にしないと、javascriptにredなんかないよと渓谷が出る
+          //これはjavascriptのオブジェクトなのでなので、ここにここに直接かく以外に、変数を定義しその中に確やり方※1もある
           { color: "red" }
         }
       >
         こんにちは
       </h1>
-      <p>おげんきですか</p>
+      <p style={contentStyle}>おげんきですか</p>
       <button
         onClick={
           //JSxのタグの中で波括弧でくくると、その中はjavascriptがかける
